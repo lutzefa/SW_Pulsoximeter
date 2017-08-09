c6788ce08d4d60a2ee62057a1de727c69b612ed6
Lutz Willert <lutzwillert@gmx.net>
2017-08-09 17:44:21 +0200

    #1 Upload der Demosoftware & unseres Clones benelu_v1.0 zur Bearbeitung

---
+++ STK3800_biometric/GNU ARM v4.9.3 - Debug/Drivers/si114x_functions.d
@@ -0,0 +1,8 @@
+Drivers/si114x_functions.o: \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.1/hardware/kit/common/drivers/si114x_functions.c \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.1/hardware/kit/common/drivers/si114x_functions.h \
+ C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.1/hardware/kit/common/drivers/si114x_types.h
+
+C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.1/hardware/kit/common/drivers/si114x_functions.h:
+
+C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/gecko_sdk_suite/v1.1/hardware/kit/common/drivers/si114x_types.h:
